afcafe7c4506ed322e4c2dd0f6afba5f5612c932
OneOfOne <oneofone@gmail.com>
2016-07-04 20:37:59 +0200

    clean up the gzip backend

--- opts.go
+++ opts.go
@@ -47,25 +47,17 @@ func (dummyBackend) New() Backend { return dummyBackend{} }
 func JSONBackend() Backend { return &jsonBackend{} }
 
 type jsonBackend struct {
-	enc   *json.Encoder
-	dec   *json.Decoder
-	flush func() error
+	enc *json.Encoder
+	dec *json.Decoder
 }
 
 func (j *jsonBackend) Init(w io.Writer, r io.Reader) error {
 	j.enc, j.dec = json.NewEncoder(w), json.NewDecoder(r)
-	if f, ok := w.(flusher); ok {
-		j.flush = f.Flush
-	}
+
 	return nil
 }
 
-func (j *jsonBackend) Flush() error {
-	if j.flush != nil {
-		return j.flush()
-	}
-	return nil
-}
+func (j *jsonBackend) Flush() error { return nil }
 
 func (j *jsonBackend) Encode(v interface{}) error { return j.enc.Encode(v) }
 func (j *jsonBackend) Decode(v interface{}) error { return j.dec.Decode(v) }
@@ -76,7 +68,7 @@ func (j *jsonBackend) Unmarshal(in []byte, out interface{}) error { return json.
 // GZipBackend is an alias for  GZipLevelBackend(be, gzip.DefaultCompression)
 func GZipBackend(be Backend) Backend { return GZipLevelBackend(be, gzip.DefaultCompression) }
 
-// GZipLevelBackend returns a wrapper backend where all the data is gziped.
+// GZipLevelBackend returns a wrapper backend where all the data is gzip'ed.
 func GZipLevelBackend(be Backend, level int) Backend { return &gzipBackend{level: level, be: be} }
 
 type gzipBackend struct {
@@ -97,7 +89,12 @@ func (g *gzipBackend) Init(w io.Writer, r io.Reader) error {
 	return g.be.Init(g.gzw, gzr)
 }
 
-func (g *gzipBackend) Flush() error { return g.be.Flush() }
+func (g *gzipBackend) Flush() error {
+	if err := g.be.Flush(); err != nil {
+		return err
+	}
+	return g.gzw.Flush()
+}
 
 func (g *gzipBackend) Encode(v interface{}) error { return g.be.Encode(v) }
 func (g *gzipBackend) Decode(v interface{}) error { return g.be.Decode(v) }
@@ -109,6 +106,8 @@ func (j *gzipBackend) Close() error {
 	return j.gzw.Close()
 }
 
+// GZipJSONBackend is a shorthand for GZipBackend(JSONBackend())
 func GZipJSONBackend() Backend { return GZipBackend(JSONBackend()) }
 
+// GZipLevelJSONBackend is a shorthand for GZipLevelBackend(JSONBackend(), level)
 func GZipLevelJSONBackend(level int) Backend { return GZipLevelBackend(JSONBackend(), level) }
